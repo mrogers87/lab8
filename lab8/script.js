643fda8aafc5c83b07a1a8943ed58e91a669a5c9
Michael Rogers <michaelarogers87@gmail.com>
2017-02-14 20:56:12 -0500

    Add files via upload

---
+++ lab8/script.js
@@ -0,0 +1,18 @@
+//Create several grocery item objects with properties name and price and store the grocery items in an array.
+var shoppingList = [
+{name: "Cherries", price: "$2.99"},
+{name: "Cereal", price: "$3.99"},
+{name: "Milk", price: "$2.49"},
+];
+//loop through the array and print onut the name and price on a new line
+for(var i=0; i<shoppingList.length; i++){
+	console.log(shoppingList[i].name + " = " + shoppingList[i].price);
+}
+//total up the amount with the label "Total"
+var total = [2.99,3.99,2.49];
+var indexValue = total.length;
+var totalSum = 0;
+for(var x = 0; x <indexValue; x++){
+    totalSum += total[x];
+}
+console.log("Total: $"+totalSum);
